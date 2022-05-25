7711da8b60e7dc5fefe01e342e22b5370a925c8d
robertuptc <robertuptc@gmail.com>
2022-05-25 19:09:18 -0400

    finished exercise

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,25 @@
 def to_roman(num):
-    # write your code here!
+    output = ""
+    romman_numeral_to_arabic = { 
+        'M': 1000, 
+        'CM': 900, 
+        'D': 500, 
+        'CD': 400, 
+        'C': 100, 
+        'XC': 90, 
+        'L': 50, 
+        'XL': 40, 
+        'X': 10, 
+        'IX': 9, 
+        'V': 5, 
+        'IV': 4, 
+        'I': 1 
+    }
+    for key in romman_numeral_to_arabic:
+        while num >= romman_numeral_to_arabic[key]:
+            output += key
+            num -= romman_numeral_to_arabic[key]
+    return output
+
+
+
